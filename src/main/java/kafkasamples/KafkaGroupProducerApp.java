be514f31904b395e7148fd950a1aa3eaa3bb3efb
ravikant <ravi.pathak918@gmail.com>
2019-12-30 01:06:06 +0530

    added consumer groups and more examples

---
+++ src/main/java/kafkasamples/KafkaGroupProducerApp.java
@@ -0,0 +1,28 @@
+package kafkasamples;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class KafkaGroupProducerApp {
+
+	public static void main(String[] args) {
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "localhost:9092,localhost:9093");
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+		KafkaProducer<String, String> myProducer = new KafkaProducer<String, String>(props);
+		try {
+			for (int i = 0; i < 150; i++) {
+				myProducer.send(new ProducerRecord<String, String>("my-topic", "asdfghjkllkjhg"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			myProducer.close();
+		}
+	}
+
+}
